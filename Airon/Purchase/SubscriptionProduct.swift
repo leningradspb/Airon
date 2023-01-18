c2ef45fc1ca67e494c6099fcb6dba89260c5641e
Ed <Siniakov>
2023-01-18 19:13:59 +0300

    enum created

---
+++ Airon/Purchase/SubscriptionProduct.swift
@@ -0,0 +1,13 @@
+//
+//  SubscriptionProduct.swift
+//  Airon
+//
+//  Created by Eduard Kanevskii on 18.01.2023.
+//
+
+import Foundation
+
+enum SubscriptionProduct: String {
+    case week = "kanevsky.chatgpt.week.subscription"
+    case year = "kanevsky.chatgpt.1.year.subscription"
+}
