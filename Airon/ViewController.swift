371db0ddb1e7619d7e2760cafbca7678b7909eb3
Ed <Siniakov>
2023-01-18 12:56:48 +0300

    Initial Commit

---
+++ Airon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Airon
+//
+//  Created by Eduard Kanevskii on 18.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
