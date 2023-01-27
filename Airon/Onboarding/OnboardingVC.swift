bd6169cb1874cb292adb22a6c080cc0c6a60e048
Ed <Siniakov>
2023-01-27 22:13:29 +0300

    setupUI

--- Airon/Onboarding/OnboardingVC.swift
+++ Airon/Onboarding/OnboardingVC.swift
@@ -14,6 +14,7 @@ class OnboardingVC: UIViewController {
     init(state: State) {
         self.state = state
         super.init(nibName: nil, bundle: nil)
+        setupUI()
     }
     
     required init?(coder: NSCoder) {
